5315b16197356dd8790b6a2d15f9485439583db3
Ivan <vanzaj@gmail.com>
2025-02-13 20:06:14 +0800

    bash

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,7 +1,3 @@
-# Needed for all installers
-sudo apt update -y
-sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
-# Run terminal installers
 for installer in $OMAKUZ_LOCAL/install/terminal/*.sh; do source $installer; done
--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -1,10 +1,7 @@
-# Configure the bash shell using Omakub defaults
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
 cp $OMAKUZ_LOCAL/configs/bashrc ~/.bashrc
 
-# Load the PATH for use later in the installers
 source $OMAKUZ_LOCAL/defaults/bash/shell
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
-# Configure the inputrc using Omakub defaults
 cp $OMAKUZ_LOCAL/configs/inputrc ~/.inputrc
