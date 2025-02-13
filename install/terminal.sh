1ad5d7b9a13a97335c108b5769863f16858cfc70
Ivan <vanzaj@gmail.com>
2025-02-13 20:23:37 +0800

    term apps

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,3 +1,3 @@
-sudo apt install -y curl git unzip
+sudo apt install -y curl git unzip wget gpg
 
 for installer in $OMAKUZ_LOCAL/install/terminal/*.sh; do source $installer; done
--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -3,9 +3,7 @@ sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
 sudo apt update -y
 sudo apt install -y fastfetch
 
-# Only attempt to set configuration if fastfetch is not already set
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
-  # Use Omakub fastfetch config
   mkdir -p ~/.config/fastfetch
   cp $OMAKUZ_LOCAL/configs/fastfetch.jsonc ~/.config/fastfetch/config.jsonc
 fi
