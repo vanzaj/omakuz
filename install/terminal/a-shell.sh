02f5dc840bfca19ad5a75c404c0c5e367b0dbdab
Ivan <vanzaj@gmail.com>
2025-02-14 15:21:19 +0800

    move term apps to optional and inline essential apps

--- install/terminal/a-shell.sh
+++
@@ -1,7 +0,0 @@
-[ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
-cp $OMAKUZ_LOCAL/configs/bashrc ~/.bashrc
-
-source $OMAKUZ_LOCAL/defaults/bash/shell
-
-[ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
-cp $OMAKUZ_LOCAL/configs/inputrc ~/.inputrc
